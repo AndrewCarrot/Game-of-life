e7d353e60e21e7658cc936eccca5781f49e0d2d7
Andrew <hugo9987@wp.pl>
2024-11-22 17:58:38 +0100

    s

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,5 @@
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
 public class Main {
     public static void main(String[] args) {
 
-        HttpClient client = HttpClient.newHttpClient();
-
-        // Create a HttpRequest
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.github.com"))
-                .GET()
-                .build();
-
-        try {
-            // Send the request and get the response
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            // Print the response status code and body
-            System.out.println("Status code: " + response.statusCode());
-            System.out.println("Response body: " + response.body());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
